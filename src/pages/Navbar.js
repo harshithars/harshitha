269845d32b41f0e4648cf9ebc9980afdf512cdcd
R S <harshitha.r-s@capgemini.com>
2022-07-12 18:00:24 +0530

    first commit

---
+++ src/pages/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a href="https://javaguides.net" className="navbar-brand">
+            </a>
+
+            <Link to="/">Home </Link> |
+            <Link to="/allcustomers"> All Customer</Link>
+
+
+            <Outlet />
+          </div>
+
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
